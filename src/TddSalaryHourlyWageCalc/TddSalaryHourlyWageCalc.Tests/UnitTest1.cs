4630ce95dd3c6c11fc82e75e067c3457fc6fd802
codeman061988 <codyhicks57@hotmail.com>
2020-01-12 13:08:24 -0600

    Added solution, test project and requirements

---
+++ src/TddSalaryHourlyWageCalc/TddSalaryHourlyWageCalc.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TddSalaryHourlyWageCalc.Tests
+{
+    /*
+        To get hourly, divide annual salary by 2000
+        $100,006 / 2080 = $48.08 hr
+
+        To get annual, multiply hourly by 2080
+        $48.08 * $100,006.40
+            
+    */
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
