99dc63037aa019b8221c09fe5d8efd9805717fcd
codeman061988 <codyhicks57@hotmail.com>
2020-01-12 18:21:11 -0600

    Added Core project and SalaryCalculator class for the first test case

---
+++ src/TddSalaryHourlyWageCalc/TddSalaryHourlyWageCalc.Core/SalaryCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TddSalaryHourlyWageCalc.Core
+{
+    public class SalaryCalculator
+    {
+        public decimal GetAnnualSalary(decimal hourlyWage)
+        {
+            const int HoursInYear = 2080;
+            decimal annualSalary = hourlyWage * HoursInYear;
+            return annualSalary;
+        }
+    }
+}
